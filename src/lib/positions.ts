8eb622bc4ed4b4e1ad96a9f3a8fda67d5d0086df
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-08-29 06:57:06 +0200

    🐛 add scroll position to positions

--- src/lib/positions.ts
+++ src/lib/positions.ts
@@ -1,12 +1,16 @@
 export const getMiddle = (element: Element) => {
   const { x, y, width, height } = element.getBoundingClientRect()
+  const scrollX = document.documentElement.scrollLeft
+  const scrollY = document.documentElement.scrollTop
   return {
-    x: x + width / 2,
-    y: y + height / 2,
+    x: x + width / 2 + scrollX,
+    y: y + height / 2 + scrollY,
   }
 }
 
 export const getTopLeft = (element: Element) => {
   const { x, y } = element.getBoundingClientRect()
-  return { x, y }
+  const scrollX = document.documentElement.scrollLeft
+  const scrollY = document.documentElement.scrollTop
+  return { x: x + scrollX, y: y + scrollY }
 }
