a83e861812ece89a5457bbd190d9a4a365372b71
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-09-01 14:35:08 +0200

    💡 remove todo

--- src/lib/initArrowTab.ts
+++ src/lib/initArrowTab.ts
@@ -22,7 +22,6 @@ import { getByGrid } from './strategies.js'
 // TODO: Check if tab focuses the same elements as arrowTab
 // TODO: select, details, summary, iframe
 // TODO: Investigate links that don't show focus outline sometimes
-// TODO: Add cleanup method for useEffect
 
 export const initArrowTab = ({ debug = false }: { debug?: boolean } = {}) => {
   const onKeyDown = (event: KeyboardEvent) => {
