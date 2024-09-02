d3c280f9b3e7067514b4e18c603583f8a7414132
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-09-02 10:27:14 +0200

    ✨ active element blue in debug mode

--- src/lib/debugMode.ts
+++ src/lib/debugMode.ts
@@ -7,54 +7,29 @@ export const activateDebugMode = ({
 }) => {
   inDebugMode = true
 
+  if (document.activeElement) {
+    addDebugElementToDom({
+      element: document.activeElement,
+      withinReach: false,
+      content: '',
+      color: 'rgb(33 150 243 / 80%)',
+    })
+  }
+
   let counter = focusableElements.length
   let withinReachCounter = focusableElements.filter(
     (focusable) => focusable.withinReach,
   ).length
   for (const focusable of [...focusableElements].reverse()) {
-    if (!(focusable.element instanceof HTMLElement)) continue
-    const div = document.createElement('div')
-    const rect = focusable.element.getBoundingClientRect()
-    const scrollX = document.documentElement.scrollLeft
-    const scrollY = document.documentElement.scrollTop
-
-    div.style.position = 'absolute'
-    div.style.top = `${rect.top + scrollY}px`
-    div.style.left = `${rect.left + scrollX}px`
-    let width = Math.max(rect.width, 10)
-    let height = Math.max(rect.height, 10)
-    div.style.width = `${width}px`
-    div.style.height = `${height}px`
-
-    const color = focusable.withinReach
-      ? 'rgba(76 175 80 / 80%)'
-      : 'rgb(244 67 54 / 80%)'
-    div.style.color = 'white'
-    div.style.backgroundColor = color
-    div.style.zIndex = '100000'
-    div.style.display = 'flex'
-    div.style.alignItems = 'center'
-    div.style.justifyContent = 'center'
-    div.style.cursor = 'pointer'
-    div.style.borderRadius = '4px'
-
-    div.innerHTML = `<div>${counter--} ${
-      focusable.withinReach ? `(${withinReachCounter--})` : ''
-    }</div>`
-    div.dataset.arrowtab = 'debug'
-
-    // Move the click event to the focusable element
-    div.addEventListener('click', (e) => {
-      e.preventDefault()
-      e.stopPropagation()
-      console.log({
-        focusable,
-        rect,
-        debugElement: div,
-      })
+    addDebugElementToDom({
+      ...focusable,
+      content: `<div>${counter--} ${
+        focusable.withinReach ? `(${withinReachCounter--})` : ''
+      }</div>`,
+      color: focusable.withinReach
+        ? 'rgba(76 175 80 / 80%)'
+        : 'rgb(244 67 54 / 80%)',
     })
-
-    document.body.appendChild(div)
   }
 }
 
@@ -67,3 +42,49 @@ export const deactivateDebugMode = () => {
 }
 
 export const isInDebugMode = () => inDebugMode
+
+const addDebugElementToDom = (focusable: {
+  element: Element
+  withinReach: boolean
+  content: string
+  color: string
+}) => {
+  if (!(focusable.element instanceof HTMLElement)) return
+  const div = document.createElement('div')
+  const rect = focusable.element.getBoundingClientRect()
+  const scrollX = document.documentElement.scrollLeft
+  const scrollY = document.documentElement.scrollTop
+
+  div.style.position = 'absolute'
+  div.style.top = `${rect.top + scrollY}px`
+  div.style.left = `${rect.left + scrollX}px`
+  let width = Math.max(rect.width, 10)
+  let height = Math.max(rect.height, 10)
+  div.style.width = `${width}px`
+  div.style.height = `${height}px`
+
+  div.style.color = 'white'
+  div.style.backgroundColor = focusable.color
+  div.style.zIndex = '100000'
+  div.style.display = 'flex'
+  div.style.alignItems = 'center'
+  div.style.justifyContent = 'center'
+  div.style.cursor = 'pointer'
+  div.style.borderRadius = '4px'
+
+  div.innerHTML = focusable.content
+  div.dataset.arrowtab = 'debug'
+
+  // Move the click event to the focusable element
+  div.addEventListener('click', (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    console.log({
+      focusable,
+      rect,
+      debugElement: div,
+    })
+  })
+
+  document.body.appendChild(div)
+}
